6eb18c2265967ebd3684b3f12f8ef48d800de147
jiashu li <leejosh1128@gmail.com>
2018-02-15 16:42:21 -0600

    I just started this app

--- Skunk/src/SkunkApp.java
+++ Skunk/src/SkunkApp.java
@@ -1,8 +1,47 @@
+import java.util.Scanner;
 
 public class SkunkApp {
 
+	public static void main(String[] args){
+		System.out.println("-----------Welcome to the Game of Skunk-----------");
+		System.out.println("How many players do you have?");
+		System.out.println("You have");
+		Scanner scan =new Scanner(System.in);
+		
+		int number=numberOfPlayers(scan);
+		System.out.println(number);
+		int[][] playerScore = new int[number][1];
+		for(int n=0;n<number;n++){
+			
+		}
 	
-	
-	
+	}
+	public static int numberOfPlayers(Scanner scan){
+		int players=0;
+		try{
+			while(true){
+			players=scan.nextInt();
+			if(players >1){
+				System.out.println("You have "+players+ " Players.");
+				return players;
+			}else {
+				System.out.println("Please enter the right player numbers.");
+			};
+			}
+		}catch (Exception e){
+			System.out.println("You entered the wrong number. Now the game will start with 2 defalut players");
+			players=2;
+			return players;
+		}
+		
+	}
+	public int roll(){
+		int diceResult=0;
+		diceResult= (int)(6.0*Math.random())+1;
+		
+		
+		return diceResult;
+		
+	}
 	
 }
--- Skunk/test/SkunkAppTest.java
+++ Skunk/test/SkunkAppTest.java
@@ -1,12 +1,19 @@
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
 import org.junit.Test;
 
 public class SkunkAppTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void numberOfPlayers() {
+		SkunkApp myapp=new SkunkApp();
+		Scanner number = null;
+		myapp.numberOfPlayers(number);
+		number.ne
+		
+		
 	}
 
 }
