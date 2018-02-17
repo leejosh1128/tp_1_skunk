378cfbe3baca6e7e7e5f7960d5700a9fc120460f
carollqp <li005403@stthomas.edu>
2018-02-16 18:36:46 -0600

    Changed the tests

---
+++ Skunk/test/PairOfDiceTest.java
@@ -0,0 +1,54 @@
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class PairOfDiceTest
+{
+	PairOfDice pairOfDice = new PairOfDice();
+
+
+	@Test
+	public void testRoll()
+	{
+		pairOfDice.roll();
+		int diceResult1 = pairOfDice.getDice1();
+		assertFalse("The diceResult1 cannot over 6", diceResult1 > 6);
+		assertFalse("The diceResult1 cannot less than 1", diceResult1 < 1);
+		assertFalse("The diceResult1 cannot be a negtive number", diceResult1 < 0);		
+	}
+
+	@Test
+	public void testGetDice1()
+	{
+		pairOfDice.roll();
+		int diceResult1 = pairOfDice.getDice1();
+		Integer[] arr = {1, 2, 3, 4, 5, 6};	
+		assertTrue("The diceResult1 should be one of number in arr", Arrays.asList(arr).contains(diceResult1));
+	}
+
+	@Test
+	public void testGetDice2()
+	{
+		pairOfDice.roll();
+		int diceResult2 = pairOfDice.getDice2();
+		Integer[] arr = {1, 2, 3, 4, 5, 6};
+		assertTrue("The diceResult2 should be one of number in arr", Arrays.asList(arr).contains(diceResult2));
+	}
+
+	@Test
+	public void testGetTotal()
+	{
+		pairOfDice.roll();
+		int diceResult1 = pairOfDice.getDice1();
+		int diceResult2 = pairOfDice.getDice2();
+		int total = pairOfDice.getTotal();
+		if (diceResult1 == 1 || diceResult2 == 1)
+		{
+			assertEquals("The total number should be 0", 0, total);
+		}
+		else
+			assertEquals("The total number should equals diceResult1 + diceResult2", 
+					diceResult1 + diceResult2, total);
+	}
+
+}
