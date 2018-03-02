1ca408a20c34877256fed7ed7abe72095d37d427
jiashu li <leejosh1128@gmail.com>
2018-03-02 11:11:53 -0600

    Commit bug fix

--- Skunk/src/SkunkApp.java
+++ Skunk/src/SkunkApp.java
@@ -15,19 +15,11 @@ public class SkunkApp
 		Scanner scan = new Scanner(System.in);
 		String option = "";
 		boolean systemRun = true;
-<<<<<<< HEAD
 		boolean termRun=true;
-=======
-		boolean termRun = true;
->>>>>>> branch 'master' of https://github.com/leejosh1128/tp_1_skunk.git
 		int playerNumber = numberOfPlayers(scan);
 		// System.out.println(playerNumber);
 		int[] playerScore = new int[playerNumber];
-<<<<<<< HEAD
 		int termScore=0;
-=======
-		int termScore = 0;
->>>>>>> branch 'master' of https://github.com/leejosh1128/tp_1_skunk.git
 		PairOfDice pairOfDice = new PairOfDice();
 		for (int n = 0; n < playerNumber; n++)
 		{
@@ -39,15 +31,14 @@ public class SkunkApp
 				System.out.println("1. Roll the dice.");
 				System.out.println("2. End this term.");
 				option = scan.next();
-
-				// If player chooses to roll the dice, it shows the result of 2 dices and
+				
+				// If player chooses to roll the dice, it shows the result of 2 dices and 
 				// the total number
 				if (option.equals("1"))
 				{
 					pairOfDice.roll();
 					diceResult1 = pairOfDice.getDice1();
 					diceResult2 = pairOfDice.getDice2();
-<<<<<<< HEAD
 					System.out.println("Your dice-1 result is " + diceResult1 +
 							" and your dice-2 result is " + diceResult2);
 					while(termRun){
@@ -92,74 +83,20 @@ public class SkunkApp
 					
 					
 					
-=======
-					System.out.println(
-							"Your dice-1 result is " + diceResult1 + " and your dice-2 result is " + diceResult2);
-					while (termRun)
-					{
-						if (diceResult1 == 1 || diceResult2 == 1)
-						{
-							termRun = false;
-							termScore = 0;
-							System.out.println("Player " + (n + 1) + " end your term. Next Player.");
-						}
-						if (diceResult1 != 1 || diceResult2 != 1)
-						{
-							termScore = pairOfDice.getTotal() + termScore;
-							System.out.println("1. Roll the dice.");
-							System.out.println("2. End this term.");
-							option = scan.next();
-							if (option.equals("1"))
-							{
-								pairOfDice.roll();
-								diceResult1 = pairOfDice.getDice1();
-								diceResult2 = pairOfDice.getDice2();
-								System.out.println("Your dice-1 result is " + diceResult1
-										+ " and your dice-2 result is " + diceResult2);
-								if (diceResult1 != 1 || diceResult2 != 1)
-								{
-									termScore = pairOfDice.getTotal() + termScore;
-									System.out.println("This term your total score is " + termScore);
-								}
-								else
-								{
-									termScore = 0;
-									termRun = false;
-									System.out.println("Player " + (n + 1) + " end your term. Your total score is:"
-											+ playerScore[n] + "Next Player, Player " + (n + 2));
-								}
-
-							}
-							if (option.equals("2"))
-							{
-								playerScore[n] = playerScore[n] + termScore;
-								termRun = false;
-								System.out.println("Player " + (n + 1) + " end your term. Your total score is:"
-										+ playerScore[n] + "Next Player, Player" + (n + 2));
-							}
-						}
-
-					}
-
->>>>>>> branch 'master' of https://github.com/leejosh1128/tp_1_skunk.git
 					System.out.println("Your total number is " + pairOfDice.getTotal());
 
 				}
-
+				
 				// If player chooses to end this term, go to the next player
 				if (option.equals("2"))
 				{
 					systemRun = false;
 					System.out.println("Next player");
 				}
-<<<<<<< HEAD
 //				else
 //				{
 //					System.out.println("Invalid input, please enter again");
 //				}
-=======
-				
->>>>>>> branch 'master' of https://github.com/leejosh1128/tp_1_skunk.git
 			}
 		}
 
@@ -182,11 +119,7 @@ public class SkunkApp
 				{
 					System.out.println("Please enter the right player numbers.");
 				}
-<<<<<<< HEAD
 				
-=======
-
->>>>>>> branch 'master' of https://github.com/leejosh1128/tp_1_skunk.git
 			}
 		}
 		catch (Exception e)
